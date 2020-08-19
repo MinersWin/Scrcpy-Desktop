a101666c2209d86723baa2ba52053f2743571bb5
MinersWin <minerswin@minerswin.de>
2020-08-19 12:38:37 +0200

    Added Screen Rotation, Added Recording

--- scrcpy.designer.ps1
+++ scrcpy.designer.ps1
@@ -10,6 +10,14 @@ $Form1 = New-Object -TypeName System.Windows.Forms.Form
 [System.Windows.Forms.Label]$Label5 = $null
 [System.Windows.Forms.TextBox]$TextBox4 = $null
 [System.Windows.Forms.Panel]$Panel1 = $null
+[System.Windows.Forms.Panel]$Panel4 = $null
+[System.Windows.Forms.TextBox]$TextBox6 = $null
+[System.Windows.Forms.CheckBox]$CheckBox2 = $null
+[System.Windows.Forms.Panel]$Panel3 = $null
+[System.Windows.Forms.RadioButton]$RadioButton4 = $null
+[System.Windows.Forms.RadioButton]$RadioButton3 = $null
+[System.Windows.Forms.RadioButton]$RadioButton2 = $null
+[System.Windows.Forms.RadioButton]$RadioButton1 = $null
 [System.Windows.Forms.Panel]$Panel2 = $null
 [System.Windows.Forms.TextBox]$TextBox5 = $null
 [System.Windows.Forms.CheckBox]$CheckBox1 = $null
@@ -30,9 +38,19 @@ $Panel1 = (New-Object -TypeName System.Windows.Forms.Panel)
 $CheckBox1 = (New-Object -TypeName System.Windows.Forms.CheckBox)
 $Panel2 = (New-Object -TypeName System.Windows.Forms.Panel)
 $TextBox5 = (New-Object -TypeName System.Windows.Forms.TextBox)
+$Panel3 = (New-Object -TypeName System.Windows.Forms.Panel)
+$RadioButton1 = (New-Object -TypeName System.Windows.Forms.RadioButton)
+$RadioButton2 = (New-Object -TypeName System.Windows.Forms.RadioButton)
+$RadioButton3 = (New-Object -TypeName System.Windows.Forms.RadioButton)
+$RadioButton4 = (New-Object -TypeName System.Windows.Forms.RadioButton)
+$Panel4 = (New-Object -TypeName System.Windows.Forms.Panel)
+$CheckBox2 = (New-Object -TypeName System.Windows.Forms.CheckBox)
+$TextBox6 = (New-Object -TypeName System.Windows.Forms.TextBox)
 ([System.ComponentModel.ISupportInitialize]$PictureBox1).BeginInit()
 $Panel1.SuspendLayout()
 $Panel2.SuspendLayout()
+$Panel3.SuspendLayout()
+$Panel4.SuspendLayout()
 $Form1.SuspendLayout()
 #
 #Label1
@@ -127,6 +145,8 @@ $TextBox4.Text = [System.String]'15'
 #Panel1
 #
 $Panel1.AutoScroll = $true
+$Panel1.Controls.Add($Panel4)
+$Panel1.Controls.Add($Panel3)
 $Panel1.Controls.Add($Panel2)
 $Panel1.Controls.Add($TextBox1)
 $Panel1.Controls.Add($TextBox4)
@@ -170,6 +190,91 @@ $TextBox5.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([Syst
 $TextBox5.TabIndex = [System.Int32]11
 $TextBox5.Text = [System.String]'1224:1440:0:0'
 #
+#Panel3
+#
+$Panel3.BorderStyle = [System.Windows.Forms.BorderStyle]::FixedSingle
+$Panel3.Controls.Add($RadioButton4)
+$Panel3.Controls.Add($RadioButton3)
+$Panel3.Controls.Add($RadioButton2)
+$Panel3.Controls.Add($RadioButton1)
+$Panel3.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]195,[System.Int32]86))
+$Panel3.Name = [System.String]'Panel3'
+$Panel3.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]200,[System.Int32]130))
+$Panel3.TabIndex = [System.Int32]12
+#
+#RadioButton1
+#
+$RadioButton1.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]3,[System.Int32]3))
+$RadioButton1.Name = [System.String]'RadioButton1'
+$RadioButton1.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]192,[System.Int32]24))
+$RadioButton1.TabIndex = [System.Int32]0
+$RadioButton1.TabStop = $true
+$RadioButton1.Text = [System.String]'natureal Orientation'
+$RadioButton1.UseCompatibleTextRendering = $true
+$RadioButton1.UseVisualStyleBackColor = $true
+#
+#RadioButton2
+#
+$RadioButton2.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]3,[System.Int32]33))
+$RadioButton2.Name = [System.String]'RadioButton2'
+$RadioButton2.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]192,[System.Int32]24))
+$RadioButton2.TabIndex = [System.Int32]1
+$RadioButton2.TabStop = $true
+$RadioButton2.Text = [System.String]'90° counterclockwise'
+$RadioButton2.UseCompatibleTextRendering = $true
+$RadioButton2.UseVisualStyleBackColor = $true
+#
+#RadioButton3
+#
+$RadioButton3.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]3,[System.Int32]63))
+$RadioButton3.Name = [System.String]'RadioButton3'
+$RadioButton3.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]192,[System.Int32]24))
+$RadioButton3.TabIndex = [System.Int32]2
+$RadioButton3.TabStop = $true
+$RadioButton3.Text = [System.String]'180°'
+$RadioButton3.UseCompatibleTextRendering = $true
+$RadioButton3.UseVisualStyleBackColor = $true
+#
+#RadioButton4
+#
+$RadioButton4.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]3,[System.Int32]93))
+$RadioButton4.Name = [System.String]'RadioButton4'
+$RadioButton4.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]192,[System.Int32]26))
+$RadioButton4.TabIndex = [System.Int32]3
+$RadioButton4.TabStop = $true
+$RadioButton4.Text = [System.String]'90° clockwise'
+$RadioButton4.UseCompatibleTextRendering = $true
+$RadioButton4.UseVisualStyleBackColor = $true
+#
+#Panel4
+#
+$Panel4.BorderStyle = [System.Windows.Forms.BorderStyle]::FixedSingle
+$Panel4.Controls.Add($TextBox6)
+$Panel4.Controls.Add($CheckBox2)
+$Panel4.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]195,[System.Int32]222))
+$Panel4.Name = [System.String]'Panel4'
+$Panel4.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]200,[System.Int32]69))
+$Panel4.TabIndex = [System.Int32]13
+#
+#CheckBox2
+#
+$CheckBox2.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]3,[System.Int32]3))
+$CheckBox2.Name = [System.String]'CheckBox2'
+$CheckBox2.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]192,[System.Int32]24))
+$CheckBox2.TabIndex = [System.Int32]0
+$CheckBox2.Text = [System.String]'Record'
+$CheckBox2.UseCompatibleTextRendering = $true
+$CheckBox2.UseVisualStyleBackColor = $true
+#
+#TextBox6
+#
+$TextBox6.Enabled = $false
+$TextBox6.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]3,[System.Int32]33))
+$TextBox6.Name = [System.String]'TextBox6'
+$TextBox6.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]192,[System.Int32]24))
+$TextBox6.TabIndex = [System.Int32]1
+$TextBox6.Text = [System.String]'file.mp4'
+#
 #Form1
 #
 $Form1.ClientSize = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]538,[System.Int32]573))
@@ -184,6 +289,9 @@ $Panel1.ResumeLayout($false)
 $Panel1.PerformLayout()
 $Panel2.ResumeLayout($false)
 $Panel2.PerformLayout()
+$Panel3.ResumeLayout($false)
+$Panel4.ResumeLayout($false)
+$Panel4.PerformLayout()
 $Form1.ResumeLayout($false)
 Add-Member -InputObject $Form1 -Name base -Value $base -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name Label1 -Value $Label1 -MemberType NoteProperty
@@ -197,6 +305,14 @@ Add-Member -InputObject $Form1 -Name TextBox3 -Value $TextBox3 -MemberType NoteP
 Add-Member -InputObject $Form1 -Name Label5 -Value $Label5 -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name TextBox4 -Value $TextBox4 -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name Panel1 -Value $Panel1 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name Panel4 -Value $Panel4 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name TextBox6 -Value $TextBox6 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name CheckBox2 -Value $CheckBox2 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name Panel3 -Value $Panel3 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name RadioButton4 -Value $RadioButton4 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name RadioButton3 -Value $RadioButton3 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name RadioButton2 -Value $RadioButton2 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name RadioButton1 -Value $RadioButton1 -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name Panel2 -Value $Panel2 -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name TextBox5 -Value $TextBox5 -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name CheckBox1 -Value $CheckBox1 -MemberType NoteProperty
