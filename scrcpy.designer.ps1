ae6e9aa84fb1690fe67fe3b831bdc8b5a028b7e0
MinersWin <minerswin@minerswin.de>
2020-08-19 12:25:40 +0200

    Added IP Option + Max Size Option

--- scrcpy.designer.ps1
+++ scrcpy.designer.ps1
@@ -2,12 +2,18 @@ $Form1 = New-Object -TypeName System.Windows.Forms.Form
 [System.Windows.Forms.Label]$Label1 = $null
 [System.Windows.Forms.PictureBox]$PictureBox1 = $null
 [System.Windows.Forms.Label]$Label2 = $null
+[System.Windows.Forms.TextBox]$TextBox1 = $null
+[System.Windows.Forms.Label]$Label3 = $null
+[System.Windows.Forms.TextBox]$TextBox2 = $null
 function InitializeComponent
 {
 $resources = . (Join-Path $PSScriptRoot 'scrcpy.resources.ps1')
 $Label1 = (New-Object -TypeName System.Windows.Forms.Label)
 $PictureBox1 = (New-Object -TypeName System.Windows.Forms.PictureBox)
 $Label2 = (New-Object -TypeName System.Windows.Forms.Label)
+$TextBox1 = (New-Object -TypeName System.Windows.Forms.TextBox)
+$Label3 = (New-Object -TypeName System.Windows.Forms.Label)
+$TextBox2 = (New-Object -TypeName System.Windows.Forms.TextBox)
 ([System.ComponentModel.ISupportInitialize]$PictureBox1).BeginInit()
 $Form1.SuspendLayout()
 #
@@ -33,16 +39,45 @@ $PictureBox1.TabStop = $false
 #
 #Label2
 #
-$Label2.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]12,[System.Int32]151))
+$Label2.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]12,[System.Int32]115))
 $Label2.Name = [System.String]'Label2'
 $Label2.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]100,[System.Int32]23))
 $Label2.TabIndex = [System.Int32]2
-$Label2.Text = [System.String]'Label2'
+$Label2.Text = [System.String]'IP:'
 $Label2.UseCompatibleTextRendering = $true
+$Label2.add_Click($Label2_Click)
+#
+#TextBox1
+#
+$TextBox1.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]12,[System.Int32]141))
+$TextBox1.Name = [System.String]'TextBox1'
+$TextBox1.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]173,[System.Int32]24))
+$TextBox1.TabIndex = [System.Int32]3
+$TextBox1.Text = [System.String]'127.0.0.1'
+#
+#Label3
+#
+$Label3.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]12,[System.Int32]168))
+$Label3.Name = [System.String]'Label3'
+$Label3.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]100,[System.Int32]23))
+$Label3.TabIndex = [System.Int32]4
+$Label3.Text = [System.String]'Max Size:'
+$Label3.UseCompatibleTextRendering = $true
+#
+#TextBox2
+#
+$TextBox2.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]12,[System.Int32]194))
+$TextBox2.Name = [System.String]'TextBox2'
+$TextBox2.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]173,[System.Int32]24))
+$TextBox2.TabIndex = [System.Int32]5
+$TextBox2.Text = [System.String]'1080'
 #
 #Form1
 #
 $Form1.ClientSize = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]538,[System.Int32]573))
+$Form1.Controls.Add($TextBox2)
+$Form1.Controls.Add($Label3)
+$Form1.Controls.Add($TextBox1)
 $Form1.Controls.Add($Label2)
 $Form1.Controls.Add($PictureBox1)
 $Form1.Controls.Add($Label1)
@@ -51,9 +86,13 @@ $Form1.Icon = ([System.Drawing.Icon]$resources.'$this.Icon')
 $Form1.Text = [System.String]'TGF Scrcpy Client'
 ([System.ComponentModel.ISupportInitialize]$PictureBox1).EndInit()
 $Form1.ResumeLayout($false)
+$Form1.PerformLayout()
 Add-Member -InputObject $Form1 -Name base -Value $base -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name Label1 -Value $Label1 -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name PictureBox1 -Value $PictureBox1 -MemberType NoteProperty
 Add-Member -InputObject $Form1 -Name Label2 -Value $Label2 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name TextBox1 -Value $TextBox1 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name Label3 -Value $Label3 -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name TextBox2 -Value $TextBox2 -MemberType NoteProperty
 }
 . InitializeComponent
