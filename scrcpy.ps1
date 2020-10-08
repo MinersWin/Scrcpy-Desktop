29ef56a97e8bfb91a6da6230f72c70e02e80b167
MinersWin <minerswin@minerswin.de>
2020-10-08 15:26:35 +0200

    Added Ad

--- scrcpy.ps1
+++ scrcpy.ps1
@@ -1,3 +1,11 @@
+#MinersWin 2020
+#08.10.2020
+#https://miners.win
+#Tutorial: https://youtube.com/minerswin
+#YouTube: https://youtube.com/TheGeekFreaks
+#Forum: https://forum.thegeekfreaks.de
+#Website: https://thegeekfreaks.de
+#Dev: https://miners.win
 Add-Type -AssemblyName System.Windows.Forms
 . (Join-Path $PSScriptRoot 'scrcpy.designer.ps1')
 $Form1.ShowDialog()
